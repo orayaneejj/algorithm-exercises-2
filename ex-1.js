4135c253eb02acc3c46bd72453d0741ccb0c91e2
Orayanee Janjaeng <orayaneejj@gmail.com>
2024-11-12 15:59:15 +0700

    fix ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,14 @@
 function fizzBuzz(n) {
   let result = [];
-  for (let i = 1; (i <= n); i++) {
-    if (i % 3 === 0) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
       result.push("Fizz");
     } else if (i % 5 === 0) {
       result.push("Buzz");
     } else {
-      result.push(i);
+      result.push(`${i}`);
     }
   }
   return result;
